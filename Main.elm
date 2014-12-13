dfdfedf322a4754e78844624a5605182fc210223
Joe Fiorini <joe@joefiorini.com>
2014-12-12 20:25:01 -0500

    Linking between two views

--- Main.elm
+++ Main.elm
@@ -1,27 +1,45 @@
 import Html
-import Html (div, p, text, ul, a, li, h1)
+import Html (div, p, text, ul, a, li, h1, h2)
 import Html.Attributes (href)
 import Html.Events (onClick)
 import Json.Encode as Json
 import Graphics.Element (Element)
 import Signal as Signal
 
-main : Element
-main = (Html.toElement 1000 1000) view
+main : Signal.Signal Element
+main = Signal.map (\v -> Html.toElement 1000 1000 v) view
 
 type alias Route = { url: String }
 
 routeToJson : Route -> Json.Value
 routeToJson route = Json.object [ ("url", Json.string route.url) ]
 
+indexRoute : Route
+indexRoute = { url = "/" }
+
 postsRoute : Route
 postsRoute = { url = "/posts" }
 
-view : Html.Html
-view = div [] [header, body]
+noOp : Route
+noOp = { url = "" }
+
+view : Signal.Signal Html.Html
+view = Signal.map (\v -> div [] [header, v]) body
+
+index : Signal.Channel Route
+index = Signal.channel indexRoute
 
 postIndex : Signal.Channel Route
 postIndex = Signal.channel postsRoute
+
+listenForRoute : Route -> Signal.Signal String -> Signal.Signal Route
+listenForRoute r s = Signal.map (\d -> if d == r.url then postsRoute else noOp) s
+
+port visitIndex : Signal.Signal String
+port visitIndex =
+  let encodeRoute : Route -> String
+      encodeRoute r = Json.encode 0 <| routeToJson r in
+    Signal.map (encodeRoute) (Signal.subscribe index)
 
 port visitPostIndex : Signal.Signal String
 port visitPostIndex =
@@ -29,26 +47,43 @@ port visitPostIndex =
       encodeRoute r = Json.encode 0 <| routeToJson r in
     Signal.map (encodeRoute) (Signal.subscribe postIndex)
 
+port loadPostIndex : Signal.Signal String
+port loadIndex : Signal.Signal String
+
 header : Html.Html
 header =
   div [] [
     ul [] [
-      li [] [ linkToRoute "Posts" postsRoute ]
+      li [] [ linkToRoute "Home" index indexRoute ]
+    , li [] [ linkToRoute "Posts" postIndex postsRoute ]
     , li [] [ linkTo "About" "/about" ]
     , li [] [ linkTo "Colophon" "/colophon" ]
     ] ]
 
-body : Html.Html
+body : Signal.Signal Html.Html
 body =
-  div [] [
-    h1 [] [text "This is a website!"]
+  Signal.map (\v ->
+    div [] [
+      v
+    ]) <| Signal.mergeMany [postsView, containerView]
+
+containerView : Signal.Signal Html.Html
+containerView = Signal.map renderContainer (listenForRoute postsRoute loadIndex)
+
+renderContainer s = h1 [] [text "This is a website!"]
+
+postsView : Signal.Signal Html.Html
+postsView = Signal.map renderPosts (listenForRoute indexRoute loadPostIndex)
+
+renderPosts s = div [] [
+    h2 [] [text "This is posts!"]
   ]
 
 linkTo : String -> String -> Html.Html
 linkTo title url = a [ href url ] [ text title ]
 
-linkToRoute : String -> Route -> Html.Html
-linkToRoute title route = a [
+linkToRoute : String -> Signal.Channel Route -> Route -> Html.Html
+linkToRoute title channel route = a [
     href route.url
-  , onClick (Signal.send postIndex route)
+  , onClick (Signal.send channel route)
   ] [ text title ]
--- Main.js
+++ Main.js
@@ -1,12 +1,21 @@
 (function() {
 
     var app = Elm.fullscreen(Elm.Main, {
+      loadPostIndex: "",
+      loadIndex: ""
     });
 
     var router = new Router();
 
     document.addEventListener('click', function(e) {
       e.preventDefault();
+    });
+
+    app.ports.visitIndex.subscribe(function(routeS) {
+      var route = JSON.parse(routeS);
+      console.log(route);
+      window.history.pushState({}, '', route.url);
+      router.handleURL("/");
     });
 
     app.ports.visitPostIndex.subscribe(function(routeS) {
@@ -17,13 +26,22 @@
     });
 
     router.map(function(match) {
+      match("/").to("index");
       match("/posts").to("postIndex");
     });
 
     var handlers = {};
+    handlers.index = {
+      setup: function() {
+        console.log("index");
+        app.ports.loadIndex.send("string");
+      }
+    };
+
     handlers.postIndex = {
       setup: function() {
         console.log('postIndex');
+        app.ports.loadPostIndex.send("string");
       }
     };
 
@@ -34,5 +52,9 @@
     window.addEventListener('popstate', function(e) {
       router.handleURL(window.location.href);
     });
+
+    console.log("setting up router");
+
+    router.handleURL("/");
 
 })();
