873a34439cffff92dc1d1b5f451ff848e5670039
Joe Fiorini <joe@joefiorini.com>
2014-12-14 00:23:37 -0500

    Working interaction with a single resource

--- Main.elm
+++ Main.elm
@@ -2,60 +2,117 @@ import Html
 import Html (div, p, text, ul, a, li, h1, h2)
 import Html.Attributes (href)
 import Html.Events (onClick)
+import List as List
+import Dict as Dict
+import Debug as Debug
+import Result as Result
 import Json.Encode as Json
+import Json.Decode ((:=))
+import Json.Decode as JsonD
 import Graphics.Element (Element)
 import Signal as Signal
+
+import Native.Router as Router
 
 main : Signal.Signal Element
 main = Signal.map (\v -> Html.toElement 1000 1000 v) view
 
-type alias Route = { url: String }
+-------- FRAMEWORK -----------
 
-routeToJson : Route -> Json.Value
-routeToJson route = Json.object [ ("url", Json.string route.url) ]
+type RouteState = NoState
 
-indexRoute : Route
-indexRoute = { url = "/" }
+type alias Url = String
 
-postsRoute : Route
-postsRoute = { url = "/posts" }
+type alias RouteParams = Dict.Dict (String, String)
+-- type alias RouteHandler h s =
+--   { h | serialize: s -> Route -> RouteParams }
+type alias RouteHandler = String
+type alias RouteHandlerM = (RouteHandler, Json.Value)
 
-noOp : Route
-noOp = { url = "" }
+-- routeParams : List (String, String) -> RouteParams
+-- routeParams = Dict.fromList
+
+-- routeHandler = { serialize = identity }
+-- defaultRouteHandler = routeHandler {} NoState
+-- defaultRouteHandlerState s = routeHandler {} s
+
+-- type Route h s = UrlRoute String Url (RouteHandler h s)
+
+-- visitRoute : Signal.Channel (Route,s)
+-- visitRoute = Signal.channel (indexRoute,NoState)
+
+-- indexRoute : Route
+-- indexRoute = { url = "/" }
+indexRoute : RouteHandler
+indexRoute = "index"
+
+postsIndexRoute : RouteHandler
+postsIndexRoute = "postsIndex"
+
+-- routeToJson : Route -> Json.Value
+-- routeToJson route = Json.object [ ("url", Json.string route.url) ]
+
+-------- IMPLEMENTATION --------
+
+type alias Post =
+  { id : String
+  , title : String
+  , body : String
+  }
+
+-- serializePost : Post -> RouteParams
+-- serializePost p = routeParams ("id", toString p.id)
+
+-- type alias PostsRoute = RouteHandler {} (List Post)
+
+-- postsRoute : PostsRoute
+-- postsRoute = routeHandler
+
+-- postsShowRoute : RouteHandler {} Post
+-- postsShowRoute =
+--   { routeHandler | serialize <- serializePost }
 
 view : Signal.Signal Html.Html
 view = Signal.map (\v -> div [] [header, v]) body
 
-index : Signal.Channel Route
-index = Signal.channel indexRoute
+-- index = UrlRoute "index" "/" (defaultRouteHandler)
 
-postIndex : Signal.Channel Route
-postIndex = Signal.channel postsRoute
+-- postsIndex : Route
+-- postsIndex = UrlRoute "postsIndex" "/posts" (routeHandler postsRoute)
 
-listenForRoute : Route -> Signal.Signal String -> Signal.Signal Route
-listenForRoute r s = Signal.map (\d -> if d == r.url then postsRoute else noOp) s
+-- postsShow : Route
+-- postsShow = UrlRoute "postsShow" "/posts/:id" postsShowRoute
 
-port visitIndex : Signal.Signal String
-port visitIndex =
-  let encodeRoute : Route -> String
-      encodeRoute r = Json.encode 0 <| routeToJson r in
-    Signal.map (encodeRoute) (Signal.subscribe index)
+-- defaultRoute = index
 
-port visitPostIndex : Signal.Signal String
-port visitPostIndex =
-  let encodeRoute : Route -> String
-      encodeRoute r = Json.encode 0 <| routeToJson r in
-    Signal.map (encodeRoute) (Signal.subscribe postIndex)
+handleRouteChange : RouteHandler -> RouteHandler -> RouteHandler -> RouteHandler
+handleRouteChange listenRoute oldRoute newRoute =
+      if | newRoute == listenRoute -> listenRoute
+         | otherwise -> oldRoute
 
-port loadPostIndex : Signal.Signal String
-port loadIndex : Signal.Signal String
+-- listenForRoute : RouteHandler -> Signal.Signal RouteHandler
+-- listenForRoute r = Signal.map (handleRouteChange r) indexRoute routeChangeP
+
+port routeChangeP : Signal.Signal RouteHandler
+port routeChangePM : Signal.Signal RouteHandlerM
+
+port visitRouteP : Signal.Signal RouteHandler
+port visitRouteP =
+  Signal.subscribe routeChannel
+
+port visitRouteMP : Signal.Signal RouteHandlerM
+port visitRouteMP =
+  Signal.subscribe routeChannelM
+
+-- port loadPostIndex : Signal.Signal String
+-- port loadIndex : Signal.Signal String
 
 header : Html.Html
 header =
   div []
   [ ul []
-    [ li [] [ linkToRoute "Home" index indexRoute ]
-    , li [] [ linkToRoute "Posts" postIndex postsRoute ]
+    [ li [] [ linkToHome "Home" ]
+    , li [] [ linkToPosts "Posts" ]
     , li [] [ linkTo "About" "/about" ]
     , li [] [ linkTo "Colophon" "/colophon" ]
     ]
@@ -69,25 +126,163 @@ body =
   Signal.map (\v ->
     div [] [
       v
-    ]) <| Signal.mergeMany [postsIndexView, indexView]
+    ]) renderRoutes
 
-indexView : Signal.Signal Html.Html
-indexView = Signal.map renderIndex (listenForRoute postsRoute loadIndex)
+-- viewForRoute : RouteHandler -> Signal Html.Html
+-- viewForRoute handler =
+--   if | handler == indexRoute -> Debug.log "renderIndex" renderIndex
+--      | handler == postsIndexRoute -> Debug.log "renderPostsIndex" renderPostsIndex
+--      | otherwise -> text ""
 
-renderIndex s = h2 [] [text "This is index!"]
+nullPost =
+  { id = 0
+  , title = ""
+  , body = ""
+  }
 
-postsIndexView : Signal.Signal Html.Html
-postsIndexView = Signal.map renderPostsIndex (listenForRoute indexRoute loadPostIndex)
+type alias IdGuy = {id : String}
+postDecoder : JsonD.Decoder IdGuy
+postDecoder =
+  JsonD.object1 IdGuy
+    ("id" := JsonD.string)
+    -- ("title" := JsonD.string)
+    -- ("body" := JsonD.string)
 
-renderPostsIndex s = div [] [
-    h2 [] [text "This is posts!"]
+-- viewForRouteM : RouteHandlerM -> Signal Html.Html
+-- viewForRouteM (handler,modelS) =
+--   if | handler == "postsShow" ->
+--     let modelR = JsonD.decodeValue jsonToPost modelS
+--         postViewR = Result.map (Debug.log "renderPostsShow" renderPostsShow) modelR in
+--     case postViewR of
+--       Result.Ok view -> view
+--     | otherwise -> text ""
+
+renderRoutes : Signal Html.Html
+renderRoutes = Signal.mergeMany [renderIndex, renderPosts]
+
+-- renderRoute : RouteHandler -> Signal.Signal Html.Html
+-- renderRoute handler =
+--   Signal.map2 (\v (h, m) ->
+--     if | v == handler -> viewForRoute handler
+--        | h == handler -> viewForRouteM (h, m)
+--        | otherwise -> text "")
+--     routeChangeP routeChangePM
+
+render : Html.Html -> RouteHandler -> Signal Html.Html
+render view handler = Signal.map (\h -> if h == "" then text "" else view) (onRoute handler)
+
+renderM : (JsonD.Value -> Html.Html) -> RouteHandler -> Signal Html.Html
+renderM view handler = Signal.map (\(h,m) -> if h == "" then text "" else view m) (onRouteM handler)
+
+onRoute : RouteHandler -> Signal RouteHandler
+onRoute handler = Signal.keepIf ((==) handler) "" routeChangeP
+
+onRouteM : RouteHandler -> Signal RouteHandlerM
+onRouteM handler = Signal.keepIf (\(h,_) -> h == handler) ("",Json.null) routeChangePM
+
+renderPosts = Signal.mergeMany [renderPostsIndex, renderPostsShow]
+
+renderIndex = render (h2 [] [text "This is index!"]) indexRoute
+
+posts : List {title:String,body:String,id:String}
+posts =
+  [ { title = "Blah Doo Dah"
+    , body = "This is such an awesome post"
+    , id = "1"
+    }
+  , { title = "Doo Dah Dee"
+    , body = "This one isn't as good."
+    , id = "2"
+    }
   ]
+
+renderPostsIndex = render (div [] [
+    h2 [] (List.map renderPostPreview posts)
+  ]) postsIndexRoute
+
+renderPostPreview post =
+  div []
+  [ linkToPost post.title post
+  ]
+
+jsonToPost : JsonD.Value -> Post
+jsonToPost value =
+  let postR = JsonD.decodeValue postDecoder (Debug.log "value" value) in
+    case postR of
+      Result.Ok postId ->
+        List.foldl (\pp p -> if p.id == postId.id then p else pp)
+          (List.head posts)
+          (List.tail posts)
+      Result.Err s -> Debug.crash s
+
+renderPostsShow = renderM (\postJ ->
+  let post = jsonToPost postJ in
+    div []
+      [ h1 [] [text post.title]
+      , p [] [text post.body]
+      ]
+    ) "postsShow"
+
+postUrl : Post -> String
+postUrl p = "/posts" ++ (toString p.id)
+
+postToJson : Post -> Json.Value
+postToJson p =
+  Json.object
+    [ ("id", Json.string p.id)
+    , ("title", Json.string p.title)
+    , ("body", Json.string p.body)
+    ]
+
+visitRoute : RouteHandler -> Signal.Message
+visitRoute handler = Signal.send routeChannel handler
+
+routeChannel : Signal.Channel RouteHandler
+routeChannel = Signal.channel "noop"
+
+routeChannelM : Signal.Channel RouteHandlerM
+routeChannelM = Signal.channel ("noop", Json.null)
+
+visitRouteM : RouteHandlerM -> Signal.Message
+visitRouteM handler = Signal.send routeChannelM handler
 
 linkTo : String -> String -> Html.Html
 linkTo title url = a [ href url ] [ text title ]
 
-linkToRoute : String -> Signal.Channel Route -> Route -> Html.Html
-linkToRoute title channel route = a [
-    href route.url
-  , onClick (Signal.send channel route)
-  ] [ text title ]
+linkToC : String -> String -> Signal.Message -> Html.Html
+linkToC title url m =
+  a
+    [ href url
+    , onClick m
+    ]
+    [ text title ]
+
+linkToHome : String -> Html.Html
+linkToHome title =
+  a
+    [ href "/"
+    , onClick (visitRoute "index")
+    ]
+    [ text title ]
+
+linkToPosts : String -> Html.Html
+linkToPosts title =
+  a
+    [ href "/posts"
+    , onClick (visitRoute "postsIndex")
+    ]
+    [ text title ]
+
+postsShowRoute : Post -> RouteHandlerM
+postsShowRoute post = ("postsShow", postToJson post)
+
+linkToPost : String -> Post -> Html.Html
+linkToPost title post =
+  linkToC title (postUrl post) (visitRouteM <| postsShowRoute post)
+
+-- linkToRoute : String -> s -> Route -> Html.Html
+-- linkToRoute title state route =
+--   a
+--     [ href <| serializeRoute state route
+--     , onClick (Signal.send visitRoute (route,state))
+--     ] [ text title ]
--- Main.js
+++ Main.js
@@ -1,60 +1,82 @@
-(function() {
+window.app = (function() {
 
     var app = Elm.fullscreen(Elm.Main, {
-      loadPostIndex: "",
-      loadIndex: ""
+      routeChangeP: ""
+      , routeChangePM: ["", {}]
     });
 
     var router = new Router();
+    var api = {
+      updateURL: function(url) {
+        window.history.pushState({}, '', url);
+        router.handleURL(url);
+      },
+      watchURLChanges: function(cb) {
+        window.addEventListener('popstate', function(e) {
+          console.log('popped state', window.location.pathname, e);
+          cb(window.location.pathname, e);
+        });
+      }
+    };
+
+    app.router = router;
 
     document.addEventListener('click', function(e) {
       e.preventDefault();
     });
 
-    app.ports.visitIndex.subscribe(function(routeS) {
-      var route = JSON.parse(routeS);
-      console.log(route);
-      window.history.pushState({}, '', route.url);
-      router.handleURL("/");
+    app.ports.visitRouteP.subscribe(function(handler) {
+      var route = router.recognizer.generate(handler);
+      api.updateURL(route);
     });
 
-    app.ports.visitPostIndex.subscribe(function(routeS) {
-      var route = JSON.parse(routeS);
-      console.log(route);
-      window.history.pushState({}, '', route.url);
-      router.handleURL("/posts");
+    app.ports.visitRouteMP.subscribe(function(handlerM) {
+      var handlerName = handlerM[0],
+          state = handlerM[1];
+
+      var route = router.generate(handlerName, state);
+
+      api.updateURL(route);
     });
 
     router.map(function(match) {
       match("/").to("index");
-      match("/posts").to("postIndex");
+      match("/posts").to("postsIndex");
+      match("/posts/:id").to("postsShow");
     });
 
-    var handlers = {};
-    handlers.index = {
-      setup: function() {
-        console.log("index");
-        app.ports.loadIndex.send("string");
-      }
-    };
-
-    handlers.postIndex = {
-      setup: function() {
-        console.log('postIndex');
-        app.ports.loadPostIndex.send("string");
-      }
-    };
+    function defaultHandler(handlerName) {
+      return {
+        model: function(s) {
+          return s;
+        },
+        serialize: function(s) {
+          return {id: s.id};
+        },
+        setup: function(model) {
+          if(Object.keys(model).length > 0) {
+            console.log("sending routeChangePM: ", handlerName);
+            app.ports.routeChangePM.send([handlerName, model]);
+          } else {
+            console.log("sending routeChangeP: ", handlerName);
+            app.ports.routeChangeP.send(handlerName);
+          }
+        }
+      };
+    }
 
     router.getHandler = function(name) {
-      return handlers[name];
+      return defaultHandler(name);
     };
 
-    window.addEventListener('popstate', function(e) {
-      router.handleURL(window.location.href);
+    api.watchURLChanges(function(url) {
+      router.handleURL(window.location.pathname);
     });
 
     console.log("setting up router");
 
+    api.updateURL("/");
     router.handleURL("/");
 
+    return app;
 })();
